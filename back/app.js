334b1657aded27e5775e4bedb2e82d8cd3802b84
Skalpir <andryxa1049@gmail.com>
2024-04-30 01:57:32 +0300

    начало.база.тело

---
+++ back/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes); // Используйте отдельный роутер для /api/auth
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
